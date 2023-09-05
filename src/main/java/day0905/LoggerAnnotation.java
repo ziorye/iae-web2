c67f5cc6e779374e827b17caf40763ee99810161
ziorye <ziorye@gmail.com>
2023-09-05 11:59:32 +0800

    LoggerAnnotation

---
+++ src/main/java/day0905/LoggerAnnotation.java
@@ -0,0 +1,10 @@
+package day0905;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LoggerAnnotation {
+    public static void main(String[] args) {
+        log.debug("@Slf4j from lombok");
+    }
+}
