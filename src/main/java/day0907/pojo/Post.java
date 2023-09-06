e754932c21434bba764b0672046144dba1582415
ziorye <ziorye@gmail.com>
2023-09-06 15:16:17 +0800

    add O/R

---
+++ src/main/java/day0907/pojo/Post.java
@@ -0,0 +1,20 @@
+package day0907.pojo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Post {
+    long id;
+    String title;
+    String slug;
+    String cover;
+    String description;
+    String content;
+    int status;
+    long userId;
+    int viewCount;
+    Date createdAt;
+    Date updatedAt;
+}
